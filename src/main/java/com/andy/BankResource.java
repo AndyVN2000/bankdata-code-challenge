08a44d2f6e969e140d8fcaa67d783030e8f48521
andy <202007621@post.au.dk>
2025-06-28 14:06:23 +0200

    Created a GET and PATCH method in BankResource

--- src/main/java/com/andy/BankResource.java
+++ src/main/java/com/andy/BankResource.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -38,6 +40,52 @@ public class BankResource {
             statement.executeUpdate();
             System.out.println("Account created: " + account.getFirstName() + " " + account.getLastName() + ", Balance: " + account.getBalance());
             return Response.status(Response.Status.CREATED).entity(account).build();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
+    @Path("/{accountNumber}")
+    public Response getAccount(int accountNumber) {
+        String selectSQL = "SELECT * FROM EntityAccount WHERE accountNumber = ?";
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(selectSQL)) {
+            statement.setInt(1, accountNumber);
+            var resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                EntityAccount account = new EntityAccount();
+                account.setAccountNumber(resultSet.getInt("accountNumber"));
+                account.setBalance(resultSet.getDouble("balance"));
+                account.setFirstName(resultSet.getString("firstName"));
+                account.setLastName(resultSet.getString("lastName"));
+                return Response.ok(account).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response depositMoney(EntityAccount account) {
+        String updateSQL = "UPDATE EntityAccount SET balance = balance + ? WHERE accountNumber = ?";
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(updateSQL)) {
+            statement.setDouble(1, account.getBalance());
+            statement.setInt(2, account.getAccountNumber());
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Deposit successful for account number: " + account.getAccountNumber());
+                return Response.ok(account).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
